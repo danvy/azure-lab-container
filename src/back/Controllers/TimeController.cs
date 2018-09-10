03086eefc965609db54572146dc73db24ea5690d
Alex Danvy <alex@danvy.tv>
2018-09-10 23:47:22 +0200

    init

---
+++ src/back/Controllers/TimeController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace back.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TimeController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<string> Get()
+        {
+            return DateTime.Now.ToString();
+        }
+    }
+}
